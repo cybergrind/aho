ce402c42d6de359b0ce60f06ebcf87cb1d57144c
Kirill Pinchuk <k_pinchuk@wargaming.net>
2014-11-09 22:26:39 +0300

    Qsort v2

--- src/aho/ex_sort.clj
+++ src/aho/ex_sort.clj
@@ -1,6 +1,15 @@
 (ns aho.sort)
 
 
+(defn qsort_reduce
+  [[less more pivot_val] val]
+   (cond
+    (<= val pivot_val) [(conj less val) more pivot_val]
+    (> val pivot_val) [less (conj more val) pivot_val]))
+
+(qsort_reduce
+ [[1] [3] 2] 5)
+
 (defn qsort
   [to_sort]
   (cond
@@ -8,9 +17,7 @@
    :else
     (let [pivot (first to_sort)
           arr (rest to_sort)
-          less (filter #(<= % pivot) arr)
-          more (filter #(> % pivot) arr)
-          ]
+          [less more _] (reduce qsort_reduce [[] [] pivot] arr)]
       (concat (qsort less) [pivot] (qsort more)))))
 
 (let [arr [1 9 3 4 32 45 9 1123 992 123 123 123 123 123]]
