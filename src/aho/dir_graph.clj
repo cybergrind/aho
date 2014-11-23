7f73e221ea92a42e574b06e217562178238b4eb1
Kirill Pinchuk <k_pinchuk@wargaming.net>
2014-11-23 16:06:52 +0300

    Directed graph tests

---
+++ src/aho/dir_graph.clj
@@ -0,0 +1,61 @@
+(ns aho.dir_graph)
+
+
+(def test_graph
+  [{:v :a, :e [:b :d :e]}
+   {:v :b, :e [:c]}
+   {:v :c, :e [:f]},
+   {:v :f, :e [:h :g]},
+   {:v :h, :e [:g]},
+   {:v :g, :e [:h]},
+   {:v :d, :e []}
+   {:v :e, :e [:g]}])
+
+(clojure.pprint/pprint (sort-by #(get %1 :v) test_graph))
+
+(defn numbered_vertices [graph]
+  (let [sorted_vertices
+        (sort (map #(get %1 :v) graph))]
+    (zipmap sorted_vertices (iterate inc 0))))
+
+(defn numbered_graph [graph]
+  (let [num_v (numbered_vertices graph)]
+    (map #(assoc %1 :num (get num_v (get %1 :v)))
+         graph)))
+
+(clojure.pprint/pprint (numbered_graph test_graph))
+
+
+(defn mark-if [vertice label mark]
+  (cond
+   (= label (get vertice :v)) (assoc vertice :mark mark)
+   :else vertice))
+
+(defn check-mark [vertice mark]
+  (= mark (get vertice :mark)))
+
+(defn mark [graph label mark]
+  (map #(mark-if %1 label mark) graph))
+
+(defn get-label [vertice] (get vertice :v))
+
+(defn find-by-label [label graph]
+  (first (filter #(= label (get-label %1)) graph)))
+
+(defn get-subvertices [vertice graph]
+  (map #(find-by-label %1 graph) (get vertice :e)))
+
+(defn topsort
+  [graph vertice]
+  (cond
+   (and (not (nil? vertice)) (not (check-mark vertice :visited)))
+   (let [new_graph (mark graph (get-label vertice) :visited)]
+     (reduce topsort new_graph (get-subvertices vertice new_graph))
+     (println vertice))
+   :else nil))
+
+(clojure.pprint/pprint (topsort test_graph (first test_graph)))
+  
+; return matrix of graph filled with 0
+(defn null_matrix [graph]
+  (let [
